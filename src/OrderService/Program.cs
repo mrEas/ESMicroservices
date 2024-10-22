e1e9fdf05c7533c010bc9b75e39d94769224a5b2
mr_eas <mr_eas@mail.ru>
2024-10-22 17:27:07 +0300

    fixes

--- src/OrderService/Program.cs
+++ src/OrderService/Program.cs
@@ -1,11 +1,46 @@
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.OpenApi.Models;
+using OrderService.Modules.Common;
+using OrderService.Modules.Orders.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddProblemDetails();
+
+var IsProduction = builder.Environment.IsProduction();
+IsProduction = false;
+builder.Services.RegisterModules(builder.Configuration, IsProduction);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new OpenApiInfo { Title = "Order service API", Version = "v1" });
+});
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.PropertyNameCaseInsensitive = true;
+    options.SerializerOptions.PropertyNamingPolicy = null;
+    options.SerializerOptions.WriteIndented = true;
+});
+
+builder.Services.AddOutputCache(options =>
+{
+    options.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(30);
+});
+
+var app = builder.Build(); 
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
- 
+
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
+app.UseOutputCache();
+app.SeedData(IsProduction);
+app.MapEndpoints();
 
 app.Run();
